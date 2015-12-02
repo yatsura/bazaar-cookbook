b47e63c0e399a5225bdaffd865e48c949c2855ca
Paul D Mitchell <pauldmitchell28@gmail.com>
2015-12-02 07:46:22 +0000

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'bazaar'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures bazaar'
+long_description 'Installs/Configures bazaar'
+version '0.1.0'
