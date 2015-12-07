dd8b544683c2fc4349892b9a3ff29191f76ac221
Paul D Mitchell <pauldmitchell28@gmail.com>
2015-12-07 15:55:44 +0000

    Integration cookbook test

---
+++ test/fixtures/cookbooks/bazaar_test/metadata.rb
@@ -0,0 +1,9 @@
+name 'bazaar_test'
+maintainer 'Paul D Mitchell'
+maintainer_email 'paul.d.mitchell@ed.ac.uk'
+license 'Apache 2.0'
+description 'This cookbook is used with test-kitchen to test the parent, bazaar cookbook'
+version '1.0.0'
+
+depends 'bazaar'
+depends 'tar'
---
+++ test/fixtures/cookbooks/bazaar_test/recipes/lwrps.rb
@@ -0,0 +1,23 @@
+include_recipe 'bazaar'
+include_recipe 'tar'
+
+directory '/tmp/branch'
+
+tar_extract 'file://./tmp/kitchen/data/repo.tar.gz' do
+  target_dir '/tmp/branch'
+  creates '/tmp/branch/repo'
+end
+
+directory '/tmp/location'
+
+bazaar '1.0' do
+  branch '/tmp/branch/repo'
+  tag '1.0'
+  location '/tmp/location/1.0'
+end
+
+bazaar '1.1' do
+  branch '/tmp/branch/repo'
+  tag '1.1'
+  location '/tmp/location/1.1'
+end
