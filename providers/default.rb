88fb89c9e2e3423f81152c91a6c246ec79a16bf0
Paul D Mitchell <pauldmitchell28@gmail.com>
2015-12-18 09:13:25 +0000

    Added sync tests

--- providers/default.rb
+++ providers/default.rb
@@ -20,10 +20,22 @@ action :update do
   end
 end
 
+action :sync do
+  if resource_exists?
+    bazaar_update
+  else
+    bazaar_checkout
+  end
+end
+
 def load_current_resource
   @current_resource = Chef::Resource::Bazaar.new(@new_resource.location)
-  @current_resource.tag = @new_resource.tag
-  @current_resource.repository = @new_resource.repository
+  if resource_exists?
+    load_version_info @current_resource.location
+    load_tags @current_resource.location
+    if @tags.has_key?(@version_info[:revno])
+    end
+  end
 end
 
 def bazaar_checkout
@@ -38,4 +50,23 @@ def bazaar_update
   converge_by("Command: #{checkoutString}") do
     execute checkoutString
   end
+end
+
+def resource_exists?
+  false
+end
+
+def load_version_info(location)
+  cmd = Mixlib::ShellOut.new("bzr version-info --template=\"{last_revision: '{date}', clean: {clean}, revno: {revno}, revision_id: '{revision_id}', branch_nick: '{branch_nick}'}\n\" --custom #{location}")
+  cmd.run_command
+  cmd.error!
+  @version_info = eval cmd.stdout
+end
+
+def load_tags
+  cmd = Mixlib::ShellOut.new("bzr tags #{location}")
+  cmd.run_command
+  cmd.error!
+  proc = Proc.new { |a| a.split(/\s/).reverse }
+  @tags = Hash[cmd.o.split("\n").collect(&p)]
 end
---
+++ test/fixtures/cookbooks/bazaar_test/recipes/sync_unit.rb
@@ -0,0 +1,4 @@
+bazaar '/1.0' do
+  tag '1.0'
+  repository '/repo'
+end
